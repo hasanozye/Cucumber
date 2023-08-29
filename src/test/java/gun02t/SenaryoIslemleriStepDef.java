b63b81bbd6b05e3f8a5781e81592bc8e5d3c2de7
Hasan Ozyer <hasanozyer06@gmail.com>
2023-08-29 23:10:07 +0300

    Added Cucumber step definitions for the scenarios in the "matematik islemleri" feature.

---
+++ src/test/java/gun02t/SenaryoIslemleriStepDef.java
@@ -0,0 +1,32 @@
+package gun02t;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class SenaryoIslemleriStepDef {
+    int sayi;
+
+    @Given("baslangic sayisi {int} olsun")
+    public void baslangicSayisiOlsun(int basSayi) {
+        sayi = basSayi;
+
+    }
+
+    @When("kullanici {int} eklediginde")
+    public void kullaniciEklediginde(int ikinciSayi) {
+        sayi += ikinciSayi;
+    }
+
+    @Then("sonuc {int} olmalidir")
+    public void sonucOlmalidir(int ucuncuSayi) {
+        Assert.assertEquals(ucuncuSayi, sayi);
+    }
+
+    @And("kullanici {int} cikardiginda")
+    public void kullaniciCikardiginda(int cikarSayi) {
+        sayi -= cikarSayi;
+    }
+}
