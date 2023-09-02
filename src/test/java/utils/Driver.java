c5dcec1494290d67b578fb5c61aefdd23cb629ba
Hasan Ozyer <hasanozyer06@gmail.com>
2023-09-02 22:05:42 +0300

    rename change. Refactoring.

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Drivers {
+public class Driver {
 
     private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
 
