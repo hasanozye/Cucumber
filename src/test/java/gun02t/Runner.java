d8a6c18151604f3e1a0f765a0d425c4a1b91436a
Hasan Ozyer <hasanozyer06@gmail.com>
2023-09-03 16:38:56 +0300

    Created a Cucumber Runner class for the HRM login feature with DataTable, and tagged it for the "@Smoker" scenarios.

--- src/test/java/gun02t/Runner.java
+++ src/test/java/gun02t/Runner.java
@@ -5,8 +5,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/java/gun02t/SenaryoIslemleri.feature"},
-        glue = {"gun02t"}
+        features = {"src/test/java/gun03t/loginWithMap/LoginWithDataTable.feature"},
+        glue = {"gun03t/loginWithMap"},
+        tags = "@Smoker"
 )
 public class Runner extends AbstractTestNGCucumberTests {
 }
